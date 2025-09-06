79edf3c19f7b3117633429164d78c744ee6a3f53
rmekrant <145578642+rmekrant@users.noreply.github.com>
2025-09-06 21:18:52 +0100

    Add files via upload

---
+++ docs/js/script.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//     anchor.addEventListener("click", function (event) {
+//       event.preventDefault(); // Empêche le saut instantané
+
+//       const targetId = this.getAttribute("href");
+//       const targetElement = document.querySelector(targetId);
+
+//       if (targetElement) {
+//         targetElement.scrollIntoView({
+//           behavior: "smooth", // Défilement fluide
+//         });
+//       }
+//     });
+//   });
+// });
